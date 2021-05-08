80275042a389457b3957ee1d51d0303fc462a48a
Bryan Artiges <shineylogaming@gmail.com>
2021-05-08 19:37:40 +0200

    ff

---
+++ Assets/ScriptBouton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+//Script pour les boutons dans L'UI
+public class ScriptBouton : MonoBehaviour
+{
+    public bool clicked = false;
+    //Fonction du bonton jouer
+    public void Jouer(string sceneJeu)
+    {
+        SceneManager.LoadScene(sceneJeu);
+    }
+
+ 
+}
